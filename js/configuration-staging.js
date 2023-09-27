7a39437d7a2fc76b0727da71d153426727603037
bohanssen <bohanssen@gmail.com>
2023-09-27 21:00:21 +0200

    cleanup

--- js/configuration-staging.js
+++
@@ -1,5 +0,0 @@
-localStorage["aws-congnito-user-pool-id"] = "eu-west-1_4pIWIdtX3";
-localStorage["aws-congnito-app-id"] = "7acbl1r2depooht7jbgqqomcd4";
-const api_url = "https://kvwwcdmpzbeenlkuwbsy3iabr4.appsync-api.eu-west-1.amazonaws.com/graphql";
-const api_ws = "wss://kvwwcdmpzbeenlkuwbsy3iabr4.appsync-realtime-api.eu-west-1.amazonaws.com/";
-const api_key = "da2-3ssjxqthojatrg65clkxfzapye";
--- js/configuration.js
+++
@@ -1,5 +0,0 @@
-localStorage["aws-congnito-user-pool-id"] = "eu-west-1_HabFFLJsZ";
-localStorage["aws-congnito-app-id"] = "bje65i0c0hum43t0ne9ksikv0";
-const api_url = "https://hnvytymwzjgyfdpazoeyhoxlg4.appsync-api.eu-west-1.amazonaws.com/graphql";
-const api_ws = "wss://hnvytymwzjgyfdpazoeyhoxlg4.appsync-realtime-api.eu-west-1.amazonaws.com/";
-const api_key = "da2-6s2fsclu3zckfkvcnffwc6uipa";
