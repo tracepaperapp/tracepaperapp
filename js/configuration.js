107480f4bf32f443084a9f79b6f45035943bb29a
bohanssen <bohanssen@gmail.com>
2023-10-06 20:37:14 +0200

    Update configuration.js

--- js/configuration.js
+++ js/configuration.js
@@ -1,5 +1,5 @@
 localStorage["aws-congnito-user-pool-id"] = "eu-west-1_tYS8PWcm1";
 localStorage["aws-congnito-app-id"] = "5d2n96bhelirs2fouajto70vrm";
-const api_url = "https://owl7ncmqlzabtfh6sfp2enrydm.appsync-api.eu-west-1.amazonaws.com/graphql";
-const api_ws = "wss://owl7ncmqlzabtfh6sfp2enrydm.appsync-realtime-api.eu-west-1.amazonaws.com/graphql";
-const api_key = "da2-zqmzaulb3nhj5fdoh6rrbsdht4";
+const api_url = "https://otenguj7zrh3nkijmeixcfwigq.appsync-api.eu-west-1.amazonaws.com/graphql";
+const api_ws = "wss://otenguj7zrh3nkijmeixcfwigq.appsync-realtime-api.eu-west-1.amazonaws.com/graphql";
+const api_key = "da2-2s7mmtdotbhabcfxyivehamt4u";
