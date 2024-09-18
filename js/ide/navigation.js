2264c4c27e2e09c5e657b6e88c06aa8a837beb0e
bohanssen <bohanssen@gmail.com>
2024-09-18 15:45:44 +0200

    Update navigation.js

--- js/ide/navigation.js
+++ js/ide/navigation.js
@@ -72,8 +72,9 @@ window.Navigation = {
 }
 
 window.addEventListener("message", async function(event) {
-    if (event.origin !== location.origin)
-        return;
+    // if (event.origin !== location.origin)
+    //     return;
+    console.log(event);
     if (event.data.type == "popout"){
         Navigation.close(event.data.file);
     } else if (event.data.type == "diagram"){
