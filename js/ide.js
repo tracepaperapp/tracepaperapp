639ff452f077b3e3e97fa07fc23850c50c7df46a
bohanssen <bohanssen@gmail.com>
2024-09-18 21:38:20 +0200

    Update ide.js

--- js/ide.js
+++ js/ide.js
@@ -2742,8 +2742,9 @@ window.Navigation = {
 }
 
 window.addEventListener("message", async function(event) {
-    if (event.origin !== location.origin)
-        return;
+    // if (event.origin !== location.origin)
+    //     return;
+    console.log(event);
     if (event.data.type == "popout"){
         Navigation.close(event.data.file);
     } else if (event.data.type == "diagram"){
@@ -2773,6 +2774,7 @@ window.addEventListener("message", async function(event) {
         console.log(event.data);
     }
 });
+
 
 var projects = {};
 var context = {};
