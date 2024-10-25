6aabde073e290132a5e614ebd03be14b1a93a3c7
bohanssen <bohanssen@gmail.com>
2024-10-25 14:26:04 +0200

    bugfix

--- js/ide.js
+++ js/ide.js
@@ -1,4 +1,4 @@
- 
+
 window.Expression = {
     get_role_expressions: async function(){
         let files = await FileSystem.listFiles();
@@ -3201,7 +3201,8 @@ window.ModelValidator = {
             if (trigger.mapping.length === 0) {
                 this.addError(filePath, "", `Trigger ${trigger.att_source} has no mapping configured`, 'Trigger Mapping');
             } else {
-                const fields = event.field.map(x => x.att_name);
+                let fields = event.field.map(x => x.att_name);
+                fields = fields.concat(event["nested-object"].map(x => x.att_name));
                 for (const m of trigger.mapping) {
                     if (!m.att_value.startsWith("#") && !fields.includes(m.att_value)) {
                         this.addError(filePath, "", `Trigger ${trigger.att_source} maps an unknown event field [${m.att_value}] to flowvar [${m.att_target}].`, 'Trigger Mapping');
@@ -3225,6 +3226,7 @@ window.ModelValidator = {
                     continue;
                 }
                 let fields = event.field.map(x => x.att_name);
+                fields = fields.concat(event["nested-object"].map(x => x.att_name));
                 emit.mapping.forEach(m => {
                     if (!fields.includes(m.att_target)){
                         this.addError(filePath, "", `Processor maps an unknown field [${m.att_target}] to event [${emit.att_ref}]`, 'Emit Event');
@@ -3239,72 +3241,93 @@ window.ModelValidator = {
         }
     },
 
-    async validateModel(files) {
-        return;//TODO finish validation
-        this.errors = [];
-        for (const file of files.filter(x => !x.endsWith(".log") && !x.endsWith(".md"))) {
-            const type = Modeler.determine_type(file);
-
-            if (type === "readme" || type === "unknown") {
-                continue; // Skip readme and unknown files
-            }
-
-            const model = await Modeler.get(file, true); // Load the model using Modeler.get
-
-            switch (type) {
-                case "config":
-                    this.validateConfig(file, model);
-                    break;
-                case "command":
-                    this.validateCommand(file, model);
-                    break;
-                case "aggregate":
-                    this.validateAggregate(file, model);
-                    this.validateAggregateHasBehavior(file, files); // Voeg specifieke aggregate-validatie toe
-                    this.validateAggregateHasEvents(file,files);
-                    break;
-                case "behavior":
-                    await this.validateBehavior(file, model);
-                    this.validateBehaviorHasTestCase(file, model); // Voeg specifieke behavior-validatie toe
-                    break;
-                case "event":
-                    await this.validateEvent(file, model);
-                    break;
-                case "view":
-                    this.validateView(file, model);
-                    break;
-                case "projection":
-                    this.validateProjection(file, model);
-                    break;
-                case "notifier":
-                    await this.validateNotifier(file, model);
-                    break;
-                case "code":
-                    this.validateCode(file, model);
-                    break;
-                case "expression":
-                    this.validateExpression(file, model);
-                    break;
-                case "pattern":
-                    this.validatePattern(file, model);
-                    break;
-                case "scenario":
-                    this.validateScenario(file, model);
-                    break;
-                default:
-                    console.log(`No specific validation for file type: ${type}`);
-            }
-        }
-
-        let keys = [];
+    async resetValidation(){
         session.issues = [];
-        this.errors.forEach(x => {
-            let k = JSON.stringify(x);
-            if (!keys.includes(k)) {
-                keys.push(k);
-                session.issues.push(x);
+        let files = await FileSystem.listFiles();
+        ModelValidator.validateModel(files);
+    },
+    async validateModel(files) {
+        return
+        if (sessionStorage["_x_validation_enabled"] != "true"){return;}
+        console.log("Start validation");
+        this.lock = true
+        this.errors = [];
+        try{
+            for (const file of files.filter(x => !x.endsWith(".log") && !x.endsWith(".md"))) {
+                console.log("Validate:",file);
+                const type = Modeler.determine_type(file);
+
+                if (type === "readme" || type === "unknown") {
+                    continue; // Skip readme and unknown files
+                }
+
+                let model = await Modeler.get(file, true); // Load the model using Modeler.get
+                switch (type) {
+                    case "config":
+                        this.validateConfig(file, model);
+                        break;
+                    case "command":
+                        this.validateCommand(file, model);
+                        break;
+                    case "aggregate":
+                        this.validateAggregate(file, model);
+                        this.validateAggregateHasBehavior(file, files); // Voeg specifieke aggregate-validatie toe
+                        this.validateAggregateHasEvents(file,files);
+                        break;
+                    case "behavior":
+                        //await sleep(Math.floor(Math.random() * 1000 ));
+                        await this.validateBehavior(file, model);
+                        this.validateBehaviorHasTestCase(file, model); // Voeg specifieke behavior-validatie toe
+                        break;
+                    case "event":
+                        //await sleep(Math.floor(Math.random() * 1000 ));
+                        await this.validateEvent(file, model);
+                        break;
+                    case "view":
+                        await this.validateView(file, model);
+                        break;
+                    case "projection":
+                        this.validateProjection(file, model);
+                        break;
+                    case "notifier":
+                        //await sleep(Math.floor(Math.random() * 1000 ));
+                        await this.validateNotifier(file, model);
+                        break;
+                    case "code":
+                        this.validateCode(file, model);
+                        break;
+                    case "expression":
+                        this.validateExpression(file, model);
+                        break;
+                    case "pattern":
+                        this.validatePattern(file, model);
+                        break;
+                    case "scenario":
+                        this.validateScenario(file, model);
+                        break;
+                    default:
+                        console.log(`No specific validation for file type: ${type}`);
+                }
+
+                let keys = session.issues.map(x => JSON.stringify(x));
+                let bck = session.issues.map(x => JSON.stringify(x));
+                let current = [];
+                this.errors.forEach(x => {
+                    let k = JSON.stringify(x);
+                    if (!keys.includes(k)) {
+                        keys.push(k);
+                        session.issues.push(x);
+                    }
+                    current.push(k);
+                });
+                let remove = bck.filter(x => !current.includes(x));
+                session.issues = session.issues.filter(x => !remove.includes(JSON.stringify(x)));
             }
-        });
+        } catch(error) {
+            console.error(error);
+        }
+        console.log("Done");
+        console.log(this.errors);
         return this.errors;
     },
 
@@ -3327,6 +3350,7 @@ window.ModelValidator = {
         if (model.att_name) {
             this.validatePascalCase(filePath, 'att_name', model.att_name);
         }
+        console.log(model)
         if (model.handlers) {
             model.handlers.forEach(handler => {
                 if (handler.type === 'event') {
@@ -3342,6 +3366,9 @@ window.ModelValidator = {
     async validateEvent(filePath, model) {
         if (model.att_name) {
             this.validatePascalCase(filePath, 'att_name', model.att_name);
+        }
+        if (!model.field || model.field.length == 0){
+            this.addError(filePath, 'domain-event', `Event [${model.att_name}] has no fields configured`, 'Missing fields');
         }
         if (model.field) {
             model.field.forEach(field => {
@@ -3366,15 +3393,34 @@ window.ModelValidator = {
         }
     },
 
-    validateView(filePath, model) {
+    async validateView(filePath, model) {
         if (model.att_name) {
             this.validatePascalCase(filePath, 'att_name', model.att_name);
         }
+        let has_key = false
+        if (!model.field || model.field.length == 0){
+            this.addError(filePath, 'view', `View [${model.att_name}] has no fields configured`, 'Missing fields');
+        }
         if (model.field) {
             model.field.forEach(field => {
+                has_key = has_key || (field.att_pk && field.att_pk == "true");
                 this.validateCamelCase(filePath, `Field: ${field.att_name}`, field.att_name);
             });
         }
+        let has_handler = model["snapshot-handler"].length !=0 || model["custom-handler"].length != 0;
+        if (has_key && !has_handler){
+            this.addError(filePath, 'view', `View [${model.att_name}] has a primary-key configured but no data-mapper`, 'Missing data-mapper');
+        }
+        if (!has_key && has_handler){
+            this.addError(filePath, 'view', `View [${model.att_name}] has a data-mapper configured but has no primary-key`, 'Missing primary-key');
+        }
+        let has_query = model.query.length != 0;
+        if (!has_key && has_query){
+            this.addError(filePath, 'view', `View [${model.att_name}] has a query configured but has no primary-key`, 'Missing primary-key');
+        }
+        model.query.forEach(q => {
+            console.log(q);
+        });
     },
 
     validateProjection(filePath, model) {
@@ -3390,10 +3436,36 @@ window.ModelValidator = {
         if (model.trigger.length === 0) {
             this.addError(filePath, "", 'Notifier must have at least one trigger configured.', 'No Trigger');
         }
+        for (const trigger of model.trigger) {
+            let event = null;
+            if (trigger.att_source.startsWith("@")){
+                continue;
+            }
+            try{
+                event = await Modeler.get_by_name(trigger.att_source,true);
+            } catch {
+                this.addError(filePath, "", `Trigger is configured to an unknown event ${trigger.att_source}`, 'Trigger Source');
+                return;
+            }
+            if (trigger["att_key-field"] === "") {
+                this.addError(filePath, "", `Trigger ${trigger.att_source} has no business key mapping configured`, 'Trigger Business Key');
+            }
+            if (trigger.mapping.length === 0) {
+                this.addError(filePath, "", `Trigger ${trigger.att_source} has no mapping configured`, 'Trigger Mapping');
+            } else {
+                let fields = event.field.map(x => x.att_name);
+                fields = fields.concat(event["nested-object"].map(x => x.att_name));
+                for (const m of trigger.mapping) {
+                    if (!m.att_value.startsWith("#") && !fields.includes(m.att_value)) {
+                        this.addError(filePath, "", `Trigger ${trigger.att_source} maps an unknown event field [${m.att_value}] to flowvar [${m.att_target}].`, 'Trigger Mapping');
+                    }
+                }
+            }
+        }
         if (model.activity.length === 0) {
             this.addError(filePath, "", 'Notifier must have at least one activity configured.', 'No Activity');
         }
-        console.log(model);
+        //TODO: activity validators
     },
 
     validateCode(filePath, model) {
