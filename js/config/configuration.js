a0b90372e6750ba13523a9a4c088f0b7c321c315
bohanssen <bohanssen@gmail.com>
2024-12-29 00:55:35 +0100

    updated prd stack

--- js/config/configuration.js
+++ js/config/configuration.js
@@ -8,10 +8,10 @@ if (!localStorage["staging-environment"] || localStorage["staging-environment"] 
 	console.log("Connected to production");
 	stage = "production";
 	bucket = "";
-	localStorage["aws-congnito-user-pool-id"] = "eu-west-1_RyjVm4Mka";
-	localStorage["aws-congnito-app-id"] = "1j28c3eth1k38lj392el22h264";
-	localStorage["aws-congnito-ui"] = "https://tracepapertwee-production.auth.eu-west-1.amazoncognito.com";
-	api_url = "https://gff2zn67jfa3xeb45ndbt6arvi.appsync-api.eu-west-1.amazonaws.com/graphql";
-	api_ws = "wss://gff2zn67jfa3xeb45ndbt6arvi.appsync-realtime-api.eu-west-1.amazonaws.com/graphql";
-	api_key = "da2-3dyeyddz6vhltdg7hpm3u2jxne";
+	localStorage["aws-congnito-user-pool-id"] = "eu-west-1_VCBsMeURY";
+	localStorage["aws-congnito-app-id"] = "56hq9fjhgih0n079mf7ttbllke";
+	localStorage["aws-congnito-ui"] = "https://tracepapertwee-production-8yokeu.auth.eu-west-1.amazoncognito.com";
+	api_url = "https://5bxy2tiphvbixlx6so247igxw4.appsync-api.eu-west-1.amazonaws.com/graphql";
+	api_ws = "wss://5bxy2tiphvbixlx6so247igxw4.appsync-realtime-api.eu-west-1.amazonaws.com/graphql";
+	api_key = "da2-ll2qxpyshzcmveqg3nr62kl6hq";
 }
